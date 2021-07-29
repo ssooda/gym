8995900b2229a6aa5dd947e90f44215aea5df3be
ssooda <ssoo.dat.y@gmail.com>
2021-07-29 18:42:56 +0900

    delete account

--- accountapp/urls.py
+++ accountapp/urls.py
@@ -1,7 +1,7 @@
 from django.contrib.auth.views import LoginView, LogoutView
 from django.urls import path
 
-from accountapp.views import hello_world, AccountCreateView, AccountDetailView, AccountUpdateView
+from accountapp.views import hello_world, AccountCreateView, AccountDetailView, AccountUpdateView, AccountDeleteView
 
 app_name = "accountapp"  # accountapp이라는 app_name 을 명시함으로써 app_name:path_name 방식을 활용할 수 있음
 
@@ -12,6 +12,10 @@ urlpatterns = [
     path('logout/', LogoutView.as_view(), name='logout'),
 
     path('create/', AccountCreateView.as_view(), name="create"),  # 함수형에서는 함수명만 명시하면 되지만 클래스형에서는 클래스명.as_view()해야함
+
     path('detail/<int:pk>', AccountDetailView.as_view(), name='detail'),  # datail/pk 라는 정수형 데이터를 받겠다
+
     path('update/<int:pk>', AccountUpdateView.as_view(), name='update'),
+
+    path('delete/<int:pk>', AccountDeleteView.as_view(), name='delete'),
 ]
--- accountapp/views.py
+++ accountapp/views.py
@@ -5,7 +5,7 @@ from django.shortcuts import render
 
 # Create your views here.
 from django.urls import reverse, reverse_lazy
-from django.views.generic import CreateView, DetailView, UpdateView
+from django.views.generic import CreateView, DetailView, UpdateView, DeleteView
 
 from accountapp.forms import AccountUpdateForm
 from accountapp.models import HelloWorld
@@ -58,8 +58,14 @@ class AccountDetailView(DetailView):  # 개인 페이지
     template_name = 'accountapp/detail.html'
 
 
-class AccountUpdateView(UpdateView):
+class AccountUpdateView(UpdateView):  # 회원정보 수정
     model = User
     form_class = AccountUpdateForm
     success_url = reverse_lazy('accountapp:hello_world')
     template_name = 'accountapp/update.html'
+
+
+class AccountDeleteView(DeleteView):  # 회원탈퇴
+    model = User
+    success_url = reverse_lazy('accountapp:login')
+    template_name = 'accountapp/delete.html'
